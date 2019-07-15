9078363a95011aee5b60a3a8e5ac909915f8941c
macymuhia <macymuhia@gmail.com>
2019-07-15 15:34:34 +0300

    Add a property to hold user details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { GithubUser } from '../app/igithub-user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'explore';
+
+    public githubUser1: GithubUser;
+
+    constructor() {
+        this.githubUser1 = new GithubUser(false, null, '');
+    }
 }
