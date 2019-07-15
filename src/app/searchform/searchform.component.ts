0a4ce0654497dc327374555677805542db9aa785
macymuhia <macymuhia@gmail.com>
2019-07-15 16:06:12 +0300

    Add methods to search user

---
+++ src/app/searchform/searchform.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
+import { GithubService } from '../github.service';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { GithubUser } from '../igithub-user';
+import { from } from 'rxjs';
+
+@Component({
+    selector: 'app-searchform',
+    templateUrl: './searchform.component.html',
+    styleUrls: ['./searchform.component.css']
+
+})
+
+
+export class SearchformComponent implements OnInit {
+    @Input() githubUser: GithubUser;
+    @Output() userUpdated: EventEmitter<GithubUser> = new EventEmitter<GithubUser>();
+
+    constructor(private _githubService: GithubService) {
+        
+    }
+
+    ngOnInit() {
+
+        if (this.githubUser) {
+            this.githubUser.user = false;
+            this.getUserInformation();
+        }
+
+    }
+
+    searchUser() {
+        if (this.githubUser.userName && this.githubUser.userName.length > 0) {
+            this._githubService.updateUser(this.githubUser.userName);
+            this.getUserInformation();
+        } else {
+            this.githubUser.user = false;
+        }
+    }
+
+    getUserInformation() {
+        if (this.githubUser.userName && this.githubUser.userName.length > 0) {
+
+            this._githubService.getUser().pipe(debounceTime(2000)).subscribe(user => {
+                this.githubUser.user = user;
+                this.userUpdated.emit(this.githubUser);
+            },
+                (err) => {
+                    console.log('err:' + err);
+                    this.githubUser.user = false;
+                },
+                () => console.log('Done')
+            );
+
+
+
+            this._githubService.getRepos().pipe(debounceTime(2000)).subscribe(repos => {
+                // console.log(repos);
+                this.githubUser.repos = repos;
+                this.userUpdated.emit(this.githubUser);
+            },
+                (err) => {
+                    console.log('err:' + err);
+                    this.githubUser.user = false;
+                },
+                () => console.log('Done')
+            );
+
+        }
+    }
+}
